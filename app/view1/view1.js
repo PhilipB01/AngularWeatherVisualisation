e8b2c4667fa2921e056dcb56c2c7e7bc04f157f8
Phil <pb2915@ic.ac.uk>
2017-01-16 02:45:29 +0000

    Working on directive

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var view1 = angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider, $httpProvider) {
   $routeProvider.when('/view1', {
@@ -16,7 +16,6 @@ var view1 = angular.module('myApp.view1', ['ngRoute'])
         statusText: "Wait",
         status: 800
   };
-  $scope.data = [];
   $scope.columns = [];
 
   dataService.getExcelWeatherData().then(function(dataResponse) {
@@ -35,8 +34,9 @@ var view1 = angular.module('myApp.view1', ['ngRoute'])
       }
     });
     $scope.columns = data.columns;
+    // reduce to hourly averages
     $scope.data = reduceData(data);
-    $scope.dataDaily = processDataDaily($scope.result.data);
+    //$scope.dataDaily = processDataDaily($scope.result.data);
   });
 
   // reduce data with d3
@@ -46,7 +46,7 @@ var view1 = angular.module('myApp.view1', ['ngRoute'])
         .key(function(d) { return d.dateTime.substring(0, 13); })
         .rollup(function(v) { return {
           pressure: d3.mean(v, function(d) { return d.pressure; }),
-          rainfall: d3.mean(v, function(d) { return d.rainfall; }),
+          rainfall: d3.sum(v, function(d) { return d.rainfall; }),
           windSpeed: d3.mean(v, function(d) { return d.windSpeed; }),
           windDirection: d3.mean(v, function(d) { return d.windDirection; }),
           temperature: d3.mean(v, function(d) { return d.temperature; }),
@@ -171,5 +171,241 @@ var view1 = angular.module('myApp.view1', ['ngRoute'])
       dataType: 'csv',
       headers: {}
     });
+  }
+})
+
+.directive('weatherVisualization', function () {
+
+  // constants
+  var margin = 20,
+      width = 960,
+      height = 500 - .5 - margin,
+      color = d3.interpolateRgb("#f77", "#77f");
+
+  return {
+    restrict: 'E',
+    scope: {
+      val: '=',
+      grouped: '='
+    },
+    link: function (scope, element, attrs) {
+
+      // set up initial svg object
+      var vis = d3.select(element[0])
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height + margin + 100);
+
+      var svg = d3.select("svg");
+
+      scope.$watch('val', function (newVal, oldVal) {
+
+        // clear the elements inside of the directive
+        vis.selectAll('*').remove();
+
+        // if 'val' is undefined, exit
+        if (!newVal) {
+          return;
+        }
+
+        var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+            y = d3.scaleLinear().rangeRound([height, 0]);
+
+        var g = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(data.map(function(d) { return d.letter; }));
+        y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+        g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(y).ticks(10, "%"))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Frequency");
+
+        g.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.letter); })
+            .attr("y", function(d) { return y(d.frequency); })
+            .attr("width", x.bandwidth())
+            .attr("height", function(d) { return height - y(d.frequency); });
+
+        // Based on: http://mbostock.github.com/d3/ex/stack.html
+        var n = newVal.length, // number of layers
+            m = newVal[0].length, // number of samples per layer
+            data = d3.layout.stack()(newVal);
+
+        var mx = m,
+            my = d3.max(data, function(d) {
+              return d3.max(d, function(d) {
+                return d.y0 + d.y;
+              });
+            }),
+            mz = d3.max(data, function(d) {
+              return d3.max(d, function(d) {
+                return d.y;
+              });
+            }),
+            x = function(d) { return d.x * width / mx; },
+            y0 = function(d) { return height - d.y0 * height / my; },
+            y1 = function(d) { return height - (d.y + d.y0) * height / my; },
+            y2 = function(d) { return d.y * height / mz; }; // or `my` not rescale
+
+        // Layers for each color
+        // =====================
+
+        var layers = vis.selectAll("g.layer")
+            .data(data)
+            .enter().append("g")
+            .style("fill", function(d, i) {
+              return color(i / (n - 1));
+            })
+            .attr("class", "layer");
+
+        // Bars
+        // ====
+
+        var bars = layers.selectAll("g.bar")
+            .data(function(d) { return d; })
+            .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) {
+              return "translate(" + x(d) + ",0)";
+            });
+
+        bars.append("rect")
+            .attr("width", x({x: .9}))
+            .attr("x", 0)
+            .attr("y", height)
+            .attr("height", 0)
+            .transition()
+            .delay(function(d, i) { return i * 10; })
+            .attr("y", y1)
+            .attr("height", function(d) {
+              return y0(d) - y1(d);
+            });
+
+        // X-axis labels
+        // =============
+
+        var labels = vis.selectAll("text.label")
+            .data(data[0])
+            .enter().append("text")
+            .attr("class", "label")
+            .attr("x", x)
+            .attr("y", height + 6)
+            .attr("dx", x({x: .45}))
+            .attr("dy", ".71em")
+            .attr("text-anchor", "middle")
+            .text(function(d, i) {
+              return d.date;
+            });
+
+        // Chart Key
+        // =========
+
+        var keyText = vis.selectAll("text.key")
+            .data(data)
+            .enter().append("text")
+            .attr("class", "key")
+            .attr("y", function (d, i) {
+              return height + 42 + 30*(i%3);
+            })
+            .attr("x", function (d, i) {
+              return 155 * Math.floor(i/3) + 15;
+            })
+            .attr("dx", x({x: .45}))
+            .attr("dy", ".71em")
+            .attr("text-anchor", "left")
+            .text(function(d, i) {
+              return d[0].user;
+            });
+
+        var keySwatches = vis.selectAll("rect.swatch")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "swatch")
+            .attr("width", 20)
+            .attr("height", 20)
+            .style("fill", function(d, i) {
+              return color(i / (n - 1));
+            })
+            .attr("y", function (d, i) {
+              return height + 36 + 30*(i%3);
+            })
+            .attr("x", function (d, i) {
+              return 155 * Math.floor(i/3);
+            });
+
+
+        // Animate between grouped and stacked
+        // ===================================
+
+        function transitionGroup() {
+          vis.selectAll("g.layer rect")
+              .transition()
+              .duration(500)
+              .delay(function(d, i) { return (i % m) * 10; })
+              .attr("x", function(d, i) { return x({x: .9 * ~~(i / m) / n}); })
+              .attr("width", x({x: .9 / n}))
+              .each("end", transitionEnd);
+
+          function transitionEnd() {
+            d3.select(this)
+                .transition()
+                .duration(500)
+                .attr("y", function(d) { return height - y2(d); })
+                .attr("height", y2);
+          }
+        }
+
+        function transitionStack() {
+          vis.selectAll("g.layer rect")
+              .transition()
+              .duration(500)
+              .delay(function(d, i) { return (i % m) * 10; })
+              .attr("y", y1)
+              .attr("height", function(d) {
+                return y0(d) - y1(d);
+              })
+              .each("end", transitionEnd);
+
+          function transitionEnd() {
+            d3.select(this)
+                .transition()
+                .duration(500)
+                .attr("x", 0)
+                .attr("width", x({x: .9}));
+          }
+        }
+
+        // reset grouped state to false
+        scope.grouped = false;
+
+        // setup a watch on 'grouped' to switch between views
+        scope.$watch('grouped', function (newVal, oldVal) {
+          // ignore first call which happens before we even have data from the Github API
+          if (newVal === oldVal) {
+            return;
+          }
+          if (newVal) {
+            transitionGroup();
+          } else {
+            transitionStack();
+          }
+        });
+      });
+    }
   }
 });
